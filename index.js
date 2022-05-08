d4ef1ebb61f366d0d58dab6cfd1c13157c606e69
Amit DebNath <amitdebnath@Amits-MacBook-Pro.local>
2022-05-08 16:07:33 +0600

    dotEnv setup

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require('dotenv').config();
 const port = process.env.PORT || 3005;
 
 
 app.use(express.json());
 app.use(cors());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
 const uri =
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.izlwf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
